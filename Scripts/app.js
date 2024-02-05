e8caebe1841f9d43fd1869a4c2354fab95fdccb4
adaml09 <adamleblancleafs@gmail.com>
2024-02-05 14:06:40 -0500

    Started work on the script file with some basic varriables needed for the HTML manipulation. As well as tweaked some of the class names to better work with the script file.

--- Scripts/app.js
+++ Scripts/app.js
@@ -4,3 +4,19 @@
  * Date Updated: 2024-02-05
  * Version: 1.0
  */
+
+// creating consants for all the heading elemnts
+const headingIndex = document.getElementsByClassName('.indexHeading');
+const headingAbout = document.getElementsByClassName('.headingAbout');
+const headingContact = document.getElementsByClassName('.headingContact');
+const headingServices = document.getElementsByClassName('.headingService');
+const headingProducts = document.getElementsByClassName('.headingProducts');
+
+// creating variables for the heading text to be injected into the elements
+const headingIndexText = "Lab 1 - Adam LeBlanc";
+const headingAboutText = "About Me";
+const headingContactText = "Contact Me";
+const headingServicesText = "Services";
+const headingProductsText = "Products";
+
+// Code to inject the text into the h1 tags of the corrosponding HTML pages
