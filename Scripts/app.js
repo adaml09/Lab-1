3ed9830f576c60dd40d968d6d5d610ae7157268b
adaml09 <adamleblancleafs@gmail.com>
2024-02-05 21:40:05 -0500

    Timer for sedning to hompage now works. Had to move things above the function in the app.js.

--- Scripts/app.js
+++ Scripts/app.js
@@ -13,11 +13,27 @@ bottomNav[0].innerHTML = "&copy 2024 Adam LeBlanc";
 var headingIndex = document.getElementsByTagName('h1');
 headingIndex[0].innerHTML = "Lab 1 - Adam LeBlanc";
 
+// getting the element for the welocme message
+var welcome = document.getElementsByName('welcome')[0];
+welcome.innerHTML = "Welcome to my Lab-1 website ! :>";
+
+// getting the about my p element to inject the text
+let aboutMe = document.getElementById('aboutMe');
+aboutMe.innerHTML = "My name is Adam LeBlanc and I'm a 20 year old student currently in my fourth semester at Durham College for computer programming." + 
+"<br />I've grown up in the Durham Region my whole life. <br />"+
+"Some of my hobbies are Cars, video games and hockey. " +
+"I played hockey growing up which was a great way to meet people and learn about being part of a team. " + 
+"Cars are something that feels like has always been a part of me. " + 
+"Every summer I spend the weekends at car meets with my dad and friends. <br />" +
+"Volunteer work is required for high school, but for me it ended up being an experience I really enjoyed." +
+" I ran card Bingo at an old age home and it was CHAOS which made it super fun !" + 
+" It would get super competitive!";
 
 // Code to inject the text into the h1 tags of the corrosponding HTML pages
 if(document.getElementsByClassName('indexHeading')){
     headingIndex[0].innerHTML = "Lab 1 - Adam LeBlanc";
 }
+
 
 // function to console log the contact info data
 function contactLog() {
@@ -33,21 +49,15 @@ function contactLog() {
     let phoneNumber = document.getElementsByName("phoneNumber")[0].value;
     console.log(phoneNumber);
 
-    // Return false to prevent the form from submitting
+    // Set a timer before redirection
+    setTimeout(function() {
+        window.location.href = './index.html';
+    }, 3000);
+
+    // Return false to prevent the form from submitting immediately
     return false;
 }
 
-// getting the about my p element to inject the text
-var aboutME = document.getElementsByName('aboutP');
 
-// injecting the text into the p element
-aboutME[0].innerHTML = 
-"My name is Adam LeBlanc and I'm a 20 year old student currently in my fourth semester at durham college for computer programing." + 
-"<br />I've grown up in the Durham Region my whole life. <br />"+
-"Some of my hobbies are Cars, video games and hockey. " +
-"I played hockey growing up which was a great way to meet people and learn about being part of a team. " + 
-"Cars are something that feels like has always been a part of me. " + 
-"Every summer I spend the weekends at car meets with my dad and friends. <br />" +
-"Volunteer work is required for highshool, but for me it ended up being an experince I really enjoyed." +
-" I ran card Bingo at an old age home and it was CHAOS which made it super fun !" + 
-" It would get super competitive!";
+
+
